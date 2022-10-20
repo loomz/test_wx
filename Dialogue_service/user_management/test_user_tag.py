9a8b4c55a42caaf4ccf78a1c7b23bf0d0fd3b534
loomz <loomz@163.com>
2022-10-20 17:27:02 +0800

    init

--- Dialogue_service/user_management/test_user_tag.py
+++ Dialogue_service/user_management/test_user_tag.py
@@ -8,17 +8,22 @@ comm = CommonHttp('https://api.weixin.qq.com/cgi-bin')
 
 
 # 用户管理 /用户标签管理->创建标签
-@pytest.mark.parametrize('tag_name', CommonReadFile().get_data_json('test_api_user_tags_create.json'))
-def test_api_user_tags_create(tag_name, token):
+@pytest.mark.parametrize('tag_name,errcode', CommonReadFile().get_data_json('test_api_user_tags_create.json'))
+def test_api_user_tags_create(tag_name,errcode, token):
     url_tags_create ='/tags/create?access_token=%s' % token
-    print('\n tag_name=%s' % tag_name[0])
+    print('\n tag_name=%s' % tag_name)
     tags_data = {
-        "tag": {"name": tag_name[0]}
+        "tag": {"name": tag_name}
     }
     print('\n tags_data=%s' % tags_data)
     tags_response = comm.post(url_tags_create, body=tags_data)
     print('\n http状态码=%s , response.json=%s' % (tags_response.status_code, tags_response.json()))
-    assert tags_response.status_code == 200 and "tag" in tags_response.json()
+
+    if errcode == 0:
+        assert tags_response.status_code == 200 and "tag" in tags_response.json()
+    else:
+        assert tags_response.status_code == 200 and tags_response.json()['errcode'] == errcode
+
 
 
 #  获取公众号已创建的标签
@@ -32,22 +37,31 @@ def test_api_user_tag_select(token):
 
 
 # 编辑标签
-def test_api_user_tags_update(token):
+@pytest.mark.parametrize('tag_id,tag_name', CommonReadFile().get_data_json('test_api_user_tags_update.json'))
+def test_api_user_tags_update(token, tag_id, tag_name):
     url_tags_update ='/tags/update?access_token=%s' % token
     tags_data = {
-            "tag": {"id": 116, "name": "广东016"}
+            "tag": {"id": tag_id, "name": tag_name}
     }
     update_response = comm.post(url_tags_update, body=tags_data)
     print('\n http状态码=%s, response.json=%s ' % (update_response.status_code, update_response.json()))
-    assert update_response.status_code == 200 and update_response.json()['errcode'] == 0
-
-
+    errcode = update_response.json()['errcode']
+    if errcode == 0:
+        assert update_response.status_code == 200 and update_response.json()['errcode'] == errcode
+    else:
+        assert update_response.status_code == 200 and update_response.json()['errcode'] == errcode
 # 删除标签
-def test_api_user_tags_delete(token):
+@pytest.mark.parametrize('tag_id',CommonReadFile().get_data_json('test_api_user_tags_delete.json'))
+def test_api_user_tags_delete(token,tag_id):
     url_tags_delete ='/tags/delete?access_token=%s' % token
     tags_data = {
-        "tag": {"id": "146"}
+        "tag": {"id": tag_id}
     }
     delete_response = comm.post(url_tags_delete, body=tags_data)
     print('\n http状态码=%s, response.json=%s ' % (delete_response.status_code, delete_response.json()))
-    assert delete_response.status_code == 200 and delete_response.json()['errcode'] == 0
+    #assert delete_response.status_code == 200 and delete_response.json()['errcode'] == 0
+    errcode = delete_response.json()['errcode']
+    if errcode == 0:
+        assert delete_response.status_code == 200 and delete_response.json()['errcode'] == errcode
+    else:
+        assert delete_response.status_code == 200 and delete_response.json()['errcode'] == errcode
