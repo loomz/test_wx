0c1f60b424c35ef8198b5203f761f6f536f3b629
loomz <loomz@163.com>
2022-10-21 11:29:55 +0800

    修改方法名

--- Dialogue_service/user_management/test_user_tag.py
+++ Dialogue_service/user_management/test_user_tag.py
@@ -4,31 +4,29 @@ import pytest
 from common.common_read_file import CommonReadFile
 from common.common_requests import CommonHttp
 
-comm = CommonHttp('https://api.weixin.qq.com/cgi-bin')
-
 
 # 用户管理 /用户标签管理->创建标签
-@pytest.mark.parametrize('tag_name,errcode', CommonReadFile().get_data_json('test_create.json'))
-def test_create(tag_name,errcode, token):
+@pytest.mark.parametrize('tag_name,expect', CommonReadFile().get_data_json('test_user_tag_create.json'))
+def test_create(tag_name, expect, token, http):
     url_tags_create ='/tags/create?access_token=%s' % token
     print('\n tag_name=%s' % tag_name)
     tags_data = {
         "tag": {"name": tag_name}
     }
     print('\n tags_data=%s' % tags_data)
-    tags_response = comm.post(url_tags_create, body=tags_data)
+    tags_response = http.post(url_tags_create, body=tags_data)
     print('\n http状态码=%s , response.json=%s' % (tags_response.status_code, tags_response.json()))
-    if errcode == 0:
+    if expect == 0:
         assert tags_response.status_code == 200 and "tag" in tags_response.json()
     else:
-        assert tags_response.status_code == 200 and tags_response.json()['errcode'] == errcode
+        assert tags_response.status_code == 200 and tags_response.json()['errcode'] == expect
 
 
 
 #  获取公众号已创建的标签
-def test_select(token):
+def test_select(token, http):
     url_tag_select = '/tags/get?access_token=%s' % token
-    tags_select_response = comm.get(url_tag_select)
+    tags_select_response = http.get(url_tag_select)
     # 打印出来的json格式的中文数据显示异常 ,json.dumps(tags_select_response.json(), ensure_ascii=False)
     tag_select_response_new = json.dumps(tags_select_response.json(), ensure_ascii=False)
     print('\n http状态码=%s,\n response.json=%s ' % (tags_select_response.status_code, tag_select_response_new))
@@ -36,30 +34,30 @@ def test_select(token):
 
 
 # 编辑标签
-@pytest.mark.parametrize('tag_id,tag_name,tag_errcode', CommonReadFile().get_data_json('test_update.json'))
-def test_update(token, tag_id, tag_name,tag_errcode):
+@pytest.mark.parametrize('tag_id,tag_name,expect', CommonReadFile().get_data_json('test_user_tag_update.json'))
+def test_update(token, http, tag_id, tag_name, expect):
     url_tags_update ='/tags/update?access_token=%s' % token
     tags_data = {
             "tag": {"id": tag_id, "name": tag_name}
     }
-    update_response = comm.post(url_tags_update, body=tags_data)
+    update_response = http.post(url_tags_update, body=tags_data)
     print('\n http状态码=%s, response.json=%s ' % (update_response.status_code, update_response.json()))
-    if tag_errcode == 0:
+    if expect == 0:
         assert update_response.status_code == 200 and update_response.json()
     else:
-        assert update_response.status_code == 200 and update_response.json()['errcode'] == tag_errcode
+        assert update_response.status_code == 200 and update_response.json()['errcode'] == expect
 
 
 # 删除标签
-@pytest.mark.parametrize('tag_id,tag_errcode', CommonReadFile().get_data_json('test_delete.json'))
-def test_delete(token, tag_id,tag_errcode):
+@pytest.mark.parametrize('tag_id,expect', CommonReadFile().get_data_json('test_user_tag_delete.json'))
+def test_delete(token, http, tag_id,expect):
     url_tags_delete ='/tags/delete?access_token=%s' % token
     tags_data = {
         "tag": {"id": tag_id}
     }
-    delete_response = comm.post(url_tags_delete, body=tags_data)
+    delete_response = http.post(url_tags_delete, body=tags_data)
     print('\n http状态码=%s, response.json=%s ' % (delete_response.status_code, delete_response.json()))
-    if tag_errcode == 0:
+    if expect == 0:
         assert delete_response.status_code == 200 and delete_response.json()
     else:
-        assert delete_response.status_code == 200 and delete_response.json()['errcode'] == tag_errcode
+        assert delete_response.status_code == 200 and delete_response.json()['errcode'] == expect
--- conftest.py
+++ conftest.py
@@ -2,15 +2,23 @@ import pytest
 from common.common_read_file import CommonReadFile
 from common.common_requests import CommonHttp
 
-comm = CommonHttp('https://api.weixin.qq.com/cgi-bin')
+@pytest.fixture(scope='function', name='http')
+def common_http():
+    common = CommonHttp('https://api.weixin.qq.com/cgi-bin')
+    yield common
 
+'''
+如果改成module，每个测试文件都会执行一次，那每个文件获取的token不一样
+如果改成function，每个方法都会执行一次，那每个方法获取的token也不一样
+'''
 #获取token
 @pytest.fixture(scope='session', name='token')
 def access_token():
+    common = CommonHttp('https://api.weixin.qq.com/cgi-bin')
     APPID = 'wx20d87c72dc1e93bf'
     APPSECRET = 'deeced62d97f6ec9a53db64dd5dd1bf0'
     token_url = '/token?grant_type=client_credential&appid=%s&secret=%s' % (APPID, APPSECRET)
-    response_token = comm.get(token_url)
+    response_token = common.get(token_url)
     print('http状态码=%s, response.json=%s ' % (response_token.status_code, response_token.json()))
     # print('token=%s' % response_token.json()['access_token'])
     token =response_token.json()['access_token']
